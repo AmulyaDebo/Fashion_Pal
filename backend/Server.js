294a3e5e5ae8724e0e1bbaf89c5b45ace64879f0
AmulyaDebo <amulya.bangari@iiitb.ac.in>
2023-05-12 17:57:07 +0530

    Added env-local yaml file

--- backend/Server.js
+++ backend/Server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require('cors');
-
+const mongoose = require('mongoose');
 const app = express();
-const dbconnection = require("./db");
 const productsRoute = require("./routes/productsRoute");
 const userRoute = require('./routes/userRoute');
+const yaml = require('js-yaml');
+const fs = require('fs');
 
 app.use(cors({
   origin: 'http://localhost:3000',
@@ -25,15 +26,44 @@ const startServer = (port) => {
   return app.listen(port, () => console.log("Server has started"));
 };
 
-// Conditionally start the server based on the environment
-if (process.env.NODE_ENV !== 'test') {
-  const port = 5000;
-  startServer(port);
+// Read configuration from YAML file
+const environment = process.env.NODE_ENV || 'development';
+const configPath = './env-local.yaml';
+const configFile = fs.readFileSync(configPath, 'utf8');
+const config = yaml.load(configFile);
+
+const { PORT, MONGODB_URI, TEST_MONGODB_URI } = config[environment];
+
+if (!MONGODB_URI && environment !== 'test') {
+  console.log('MongoDB URI is missing');
+  process.exit(1);
 }
 
-module.exports = app;
+const mongoDBURI = environment === 'test' ? TEST_MONGODB_URI : MONGODB_URI;
 
+mongoose.connect(mongoDBURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('MongoDB connection successful');
+}).catch((error) => {
+  console.log('MongoDB connection failed:', error);
+  process.exit(1);
+});
 
+const dbconnect = mongoose.connection;
 
+dbconnect.on('error', () => {
+  console.log('MongoDB connection failed');
+});
+
+dbconnect.on('connected', () => {
+  console.log('MongoDB connection successful');
+});
+
+if (environment !== 'test') {
+  const port = PORT || 5000;
+  startServer(port);
+}
 
 module.exports = app;
