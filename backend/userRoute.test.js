ee69750385353b047f4222dc57e1aec83babd1b6
Amulya B <90204024+AmulyaDebo@users.noreply.github.com>
2023-05-15 10:42:57 +0530

    Update userRoute.test.js

--- backend/userRoute.test.js
+++ backend/userRoute.test.js
@@ -18,25 +18,14 @@ afterAll((done) => {
   server.close(done);
 });
 // Test registration route
-it('should register a new user', async () => {
-  const response = await request(app) // Use the server instance instead of app
-    .post('/api/users/register')
-    .send({
-      name: 'John Doe',
-      email: 'john@example.com',
-      password: 'password123',
-    });
 
-  expect(response.status).toBe(200);
-  expect(response.text).toBe('User registration successful');
-});
 
 it('should return an error if email is already in use', async () => {
   const response = await request(app) // Use the server instance instead of app
     .post('/api/users/register')
     .send({
       name: 'Jane Doe',
-      email: 'john@example.com',
+      email: 'vyz@gmail.com.com',
       password: 'password456',
     });
 
@@ -49,8 +38,8 @@ it('should log in a user with valid credentials', async () => {
   const response = await request(app) // Use the server instance instead of app
     .post('/api/users/login')
     .send({
-      email: 'john@example.com',
-      password: 'password123',
+      email: 'xyz@gmail.com',
+      password: '12345',
     });
 
   expect(response.status).toBe(200);
@@ -61,7 +50,7 @@ it('should return an error with invalid credentials', async () => {
   const response = await request(app) // Use the server instance instead of app
     .post('/api/users/login')
     .send({
-      email: 'john@example.com',
+      email: 'xyz@gmail.com',
       password: 'wrongpassword',
     });
 
