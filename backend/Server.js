8268c81a35aecbcd5f2baa28297e62c2e955aee8
Amulya B <90204024+AmulyaDebo@users.noreply.github.com>
2023-05-15 11:51:22 +0530

    Update Server.js

--- backend/Server.js
+++ backend/Server.js
@@ -8,47 +8,53 @@ const userRoute = require('./routes/userRoute');
 const yaml = require('js-yaml');
 const fs = require('fs');
 const morgan = require('morgan');
+const winston = require('winston');
 
-// Define a custom morgan token for :data
-morgan.token('data', (req, res) => {
-  const requestData = {
-    ipAddress: req.ip,
-    method: req.method,
-    url: req.originalUrl,
-    status: res.statusCode,
-    responseTime: res.getHeader('X-Response-Time'),
-    userAgent: req.headers['user-agent']
-  };
-  return JSON.stringify(requestData);
+// Define the custom timestamp format
+morgan.token('CUSTOM_TIMESTAMP', () => {
+  const now = new Date();
+  const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+  const timestamp = `${days[now.getUTCDay()]}, ${now.getUTCDate()} ${months[now.getUTCMonth()]} ${now.getUTCFullYear()} ${now.getUTCHours()}:${now.getUTCMinutes()}:${now.getUTCSeconds()}`;
+  return timestamp;
 });
 
-// Create a write stream for the log file
-const logStream = fs.createWriteStream('access.log', { flags: 'a' });
+// Create a custom Winston logger with a file transport
+const logger = winston.createLogger({
+  transports: [
+    new winston.transports.File({ filename: 'logs/app.log' }),
+  ],
+});
 
+// Define the custom morgan format
+const morganFormat = ':CUSTOM_TIMESTAMP GMT :method :url :status :res[content-length] - :response-time ms :data';
+
+// Create a stream for morgan to write logs
+const morganStream = {
+  write: (message) => {
+    logger.info(message.trim());
+  },
+};
+
+// Configure CORS
 app.use(cors({
   origin: ['http://localhost:3000', 'http://172.18.0.2:3000'],
-  credentials: true
+  credentials: true,
 }));
 
+// Parse JSON request bodies
 app.use(bodyParser.json());
 
-// Use morgan middleware with the custom token and log to the file
-app.use(morgan(':date[web] :method :url :status :res[content-length] - :response-time ms :data', {
-  stream: logStream
-}));
+// Configure morgan logging
+app.use(morgan(morganFormat, { stream: morganStream }));
 
-
+// Define routes
 app.use('/api/products/', productsRoute);
 app.use('/api/users/', userRoute);
-
 
 app.get("", (req, res) => {
   res.send('This is from backend');
 });
-
-const startServer = (port) => {
-  return app.listen(port, () => console.log("Server has started"));
-};
 
 // Read configuration from YAML file
 const environment = process.env.NODE_ENV || 'development';
@@ -65,6 +71,7 @@ if (!MONGODB_URI && environment !== 'test') {
 
 const mongoDBURI = environment === 'test' ? TEST_MONGODB_URI : MONGODB_URI;
 
+// Connect to MongoDB
 mongoose.connect(mongoDBURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -85,9 +92,10 @@ dbconnect.on('connected', () => {
   console.log('MongoDB connection successful');
 });
 
+// Start the server
 if (environment !== 'test') {
   const port = PORT || 5000;
-  startServer(port);
+  app.listen(port, () => console.log("Server has started"));
 }
 
 module.exports = app;
